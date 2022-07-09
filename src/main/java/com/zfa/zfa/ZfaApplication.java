af4589e762ad6cf8ad3f2f3552182007c626d471
abdouluiz <abdouluiz30@gmail.com>
2022-07-09 14:29:44 +0300

    first commit

---
+++ src/main/java/com/zfa/zfa/ZfaApplication.java
@@ -0,0 +1,13 @@
+package com.zfa.zfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zfa/zfa/ZfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfa.zfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
